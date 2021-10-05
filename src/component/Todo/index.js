05e85626027642650c1f1645caa954d8b58f8a78
Hashir Umar <hashirumar19@gmail.com>
2021-10-05 21:20:56 +0500

    added delete confirmation dialog & constant msgs

--- src/component/Todo/index.js
+++ src/component/Todo/index.js
@@ -7,21 +7,22 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Button, CardActions } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import { ALERT_EMPTY_TODO, ALERT_DELET_TODO } from "../../Constants";
 import "./index.css";
 
 const Todo = () => {
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState([]);
-  
-  const handleChange = (e)=>{
+
+  const handleChange = (e) => {
     if (e.keyCode === 13) {
       addItem();
     }
-  }
+  };
 
   const addItem = () => {
     if (!inputData) {
-      alert("plzz fill data");
+      alert(ALERT_EMPTY_TODO);
     } else {
       setItems([...items, inputData]);
       setInputData("");
@@ -30,11 +31,14 @@ const Todo = () => {
 
   // delete the items
   const deleteItem = (index) => {
-    const updateditems = items.filter((elem, ind) => {
-      return index !== ind;
-    });
+    // It will ask user to confirm delete action
+    if (window.confirm(ALERT_DELET_TODO)) {
+      const updateditems = items.filter((elem, ind) => {
+        return index !== ind;
+      });
 
-    setItems(updateditems);
+      setItems(updateditems);
+    }
   };
 
   // remove all
@@ -43,8 +47,9 @@ const Todo = () => {
   };
   return (
     <>
-      <Grid item xs={12} md={6} className='main-container' style={{margin: "0 auto"}}>
-        <Card className="main-container"
+      <Grid item xs={12} md={6} className="main-container" style={{ margin: "0 auto" }}>
+        <Card
+          className="main-container"
           sx={{ maxWidth: 345 }}
           style={{
             margin: "0 auto",
@@ -52,64 +57,34 @@ const Todo = () => {
             backgroundColor: "#F8F8FF",
           }}
         >
-      
-            <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                style={{ textAlign: "center" }}
-              >
-                Todo App
-              </Typography>
-              <Typography variant="h6" component="div">
-                <TextField
-                  id="standard-basic"
-                  label="✍ Add Items..."
-                  variant="standard"
-                  value={inputData}
-                  onKeyDown={handleChange}
-                  onChange={(e) => setInputData(e.target.value)}
-                />
-                <Button
-                  style={{ marginTop: "15px" }}
-                  size="medium"
-                  variant="contained"
-                  color="primary"
-                  onClick={() => addItem()}
-                >
-                  <AddIcon />
-                </Button>
-              </Typography>
-              <Typography >
-                {items.map((elem, id) => {
-                  return (
-                    <div key={id}>
-                      <h3 style={{display:"flex",justifyContent:"space-between"}}>
-                        {elem}
-                        <Button
-                          size="small"
-                          variant="contained"
-                          color="error"
-                          onClick={() => deleteItem(id)}
-                        >
-                          <DeleteIcon />
-                        </Button>
-                      </h3>
-                    </div>
-                  );
-                })}
-              </Typography>
-            </CardContent>
-       
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "center" }}>
+              Todo App
+            </Typography>
+            <Typography variant="h6" component="div">
+              <TextField id="standard-basic" label="✍ Add Items..." variant="standard" value={inputData} onKeyDown={handleChange} onChange={(e) => setInputData(e.target.value)} />
+              <Button style={{ marginTop: "15px" }} size="medium" variant="contained" color="primary" onClick={() => addItem()}>
+                <AddIcon />
+              </Button>
+            </Typography>
+            <Typography>
+              {items.map((elem, id) => {
+                return (
+                  <div key={id}>
+                    <h3 style={{ display: "flex", justifyContent: "space-between" }}>
+                      {elem}
+                      <Button size="small" variant="contained" color="error" onClick={() => deleteItem(id)}>
+                        <DeleteIcon />
+                      </Button>
+                    </h3>
+                  </div>
+                );
+              })}
+            </Typography>
+          </CardContent>
+
           <CardActions>
-            <Button
-              style={{ marginLeft: "25%" }}
-              size="small"
-              variant="contained"
-              color="success"
-              onClick={() => removeAll()}
-            >
+            <Button style={{ marginLeft: "25%" }} size="small" variant="contained" color="success" onClick={() => removeAll()}>
               CHECK LIST
             </Button>
           </CardActions>
