7d68430749e1b4b790ca5306d15914bd4d5ed368
Zeeshan Noor <zeeshan.noor@glowerx.com>
2021-10-06 17:52:04 +0500

    added Tooltip and Search_filter

--- src/component/Todo/index.js
+++ src/component/Todo/index.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
-import { Button, Card, Grid, IconButton, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+  Tooltip,
+} from "@mui/material";
 
 // Icons
 import { HighlightOff, DeleteOutlineOutlined } from "@mui/icons-material";
 
 // Constants
-import { ALERT_EMPTY_TODO, ALERT_DELET_TODO, ALERT_DELET_ALL_TODO } from "../../Constants";
+import {
+  ALERT_EMPTY_TODO,
+  ALERT_DELET_TODO,
+  ALERT_DELET_ALL_TODO,
+} from "../../Constants";
 
 // Custom style
 import "./index.css";
 
 const Todo = () => {
   const [inputData, setInputData] = useState("");
+  const [searchData, setSearchData] = useState("");
+
   const [items, setItems] = useState([]);
 
   const handleOnTextKeyDown = (e) => {
@@ -23,18 +37,39 @@ const Todo = () => {
   const handleOnTextChange = (targetValue) => {
     setInputData(targetValue);
   };
+  const handleOnTextSearch = (targetValue) => {
+    searchItem(targetValue);
+    setSearchData(targetValue);
+  };
 
   const renderTodoList = () => {
     return items.length > 0 ? (
       <Card className="card-full-width todo-list-container">
+        <TextField
+          fullWidth
+          label="Search Items..."
+          variant="standard"
+          value={searchData}
+          onChange={(e) => handleOnTextSearch(e.target.value)}
+        />
+
         {items.map((elem, id) => {
           return (
-            <div key={id} className="todo-item">
-              <span className="todo-item-text"> {elem}</span>
-              <IconButton size="small" variant="outlined" color="error" onClick={() => deleteItem(id)}>
-                <DeleteOutlineOutlined fontSize="small" />
-              </IconButton>
-            </div>
+            <>
+              <div key={id} className="todo-item">
+                <Tooltip title={elem}>
+                  <span className="todo-item-text"> {elem}</span>
+                </Tooltip>
+                <IconButton
+                  size="small"
+                  variant="outlined"
+                  color="error"
+                  onClick={() => deleteItem(id)}
+                >
+                  <DeleteOutlineOutlined fontSize="small" />
+                </IconButton>
+              </div>
+            </>
           );
         })}
       </Card>
@@ -46,7 +81,12 @@ const Todo = () => {
   const renderTodoListFooter = () => {
     return items.length > 0 ? (
       <Card className="card-full-width" style={{ textAlign: "center" }}>
-        <Button size="small" variant="outlined" color="error" onClick={() => removeAll()}>
+        <Button
+          size="small"
+          variant="outlined"
+          color="error"
+          onClick={() => removeAll()}
+        >
           <HighlightOff fontSize="small" /> &nbsp; RESET LIST
         </Button>
       </Card>
@@ -61,7 +101,8 @@ const Todo = () => {
     if (trimValue === "") {
       alert(ALERT_EMPTY_TODO);
     } else {
-      const newInputValue = trimValue.charAt(0).toUpperCase() + trimValue.slice(1);
+      const newInputValue =
+        trimValue.charAt(0).toUpperCase() + trimValue.slice(1);
       setItems([...items, newInputValue]);
       setInputData("");
     }
@@ -84,14 +125,43 @@ const Todo = () => {
     if (window.confirm(ALERT_DELET_ALL_TODO)) setItems([]);
   };
 
+  // search items
+  const searchItem = (index) => {
+    const value = index;
+    const searchItems = items.filter((elem, ind) => {
+      return elem.toLowerCase().includes(value);
+    });
+    setItems(searchItems);
+  };
+
   return (
-    <Grid item={true} id="main-container" container xs={10} sm={6} lg={4} style={{ margin: "0 auto" }}>
+    <Grid
+      item={true}
+      id="main-container"
+      container
+      xs={10}
+      sm={6}
+      lg={4}
+      style={{ margin: "0 auto" }}
+    >
       <Card className="card-full-width">
-        <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "center" }}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          style={{ textAlign: "center" }}
+        >
           Your daily todo's
         </Typography>
         <Typography component="div">
-          <TextField fullWidth label="✍ Add Items..." variant="standard" value={inputData} onKeyDown={handleOnTextKeyDown} onChange={(e) => handleOnTextChange(e.target.value)} />
+          <TextField
+            fullWidth
+            label="✍ Add Items..."
+            variant="standard"
+            value={inputData}
+            onKeyDown={handleOnTextKeyDown}
+            onChange={(e) => handleOnTextChange(e.target.value)}
+          />
           <small>
             Press <em>Enter</em> to add your todo to list.
           </small>
